a5cea87c80d763430efa5dfa7f437e8a14931789
XfHua <jasonHuaxf@163.com>
2020-04-02 11:01:35 +0800

    Add files via upload

---
+++ App194/App194/App194/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App194
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Editor_OnCompleted(object sender, EventArgs e)
+        {
+            //The text that is copied is on https://r12a.github.io/scripts/tutorial/summaries/arabic under sample(arabic).
+            Label.Text = Editor.Text;
+        }
+    }
+}
